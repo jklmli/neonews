3e525b588667efe04e378706df122eb51c1b90ab
598074 <jiawei.h.li@gmail.com>
2011-01-03 18:47:16 -0600

    initial commit

---
+++ neonews.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3.2
+
+##########
+
+import nntplib
+
+##########
+
+class Neonews:
+	
+	#####	
+	
+	class InvalidAuth(Exception):
+		def __init__(self, msg):
+			if msg is None:
+				self.message = 'Invalid credentials provided.'
+			else:
+				self.message = 'Invalid credentials provided: ' + msg
+
+		def __str__(self):
+			return self.message
+	
+	#####
+
+	def __init__(self, newsgroup):
+		self.newsgroup = newsgroup
+	
+	def __del__(self):
+		pass
+	
+	#####
+
+	def exit(self):
+		self.newsgroup.quit()
+
+	def getGroups(self):
+		response, self.allGroups = self.newsgroup.list()
+		print(response)
+		for group in self.allGroups:
+			print(group[0])
+						
+	def welcome(self):
+		print(self.newsgroup.getwelcome())
+def main():
+	user = input('Username?\n')
+	pw = input('Password?\n')
+
+	# returns the base newsgroup object
+	try:
+		newsgroup = Neonews(nntplib.NNTP_SSL('news.cs.illinois.edu', user=user, password=pw))
+	except nntplib.NNTPTemporaryError:
+		raise Neonews.InvalidAuth
+
+	newsgroup.welcome()
+
+	newsgroup.getGroups()
+	
+if __name__ == "__main__":
+	main()
