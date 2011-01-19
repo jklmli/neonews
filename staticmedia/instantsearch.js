39e97a04882f8923af8201c3b7c0dce729c48d80
Onur Karaman <karaman1@illinois.edu>
2011-01-19 00:11:39 -0600

    improved search performance

--- staticmedia/instantsearch.js
+++ staticmedia/instantsearch.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
-	groups = $('li');
-//Keyup fixes the issues of backspace
+	resultMap = {};
+	initList = $('li');
+	resultMap[''] = initList;
+	$('input').keyup(function(){
+		searchtext = $(this).val();
+		if(resultMap[searchtext]==undefined)
+		{
+			$('li').remove();
+			$('ul').after().html(resultMap['']);
+			query = $("li:contains("+searchtext+")");
+			$('li').remove();
+			$('ul').after().html(query);
+			resultMap[searchtext] = query;
+		}
+		else
+		{
+			$('li').remove();
+			$('ul').after().html(resultMap[searchtext]);
+		}
+	});
+});
+
+/*
+
+$(document).ready(function(){
+	initList = $('li');
 	$('input').keyup(function(){
 		searchtext = $(this).val();
 		$('li').remove();
-		$('ul').after().html(groups);
-		groupquery = $("li:contains("+searchtext+")");
+		$('ul').after().html(initList);
+		query = $("li:contains("+searchtext+")");
 		$('li').remove();
-		$('ul').after().html(groupquery);
+		$('ul').after().html(query);
 	});
 });
+*/
